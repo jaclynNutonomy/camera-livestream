31338de0d9be0f6cb9decc1888981d82aa50cae2
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-18 11:58:45 +0800

    Update

--- websocket-controls.js
+++ websocket-controls.js
@@ -10,7 +10,7 @@ var WEBSOCKET_PORT = 8085;
 
 //robo control socket
 var roboServ = require('http');
-roboApp = roboServ.createServer().listen(8085);
+roboApp = roboServ.createServer().listen(8086);
 var roboSocketServer = require('ws').Server;
 var roboWS = new roboSocketServer({server: roboApp});
 roboWS.broadcast = function(data, opts) {
