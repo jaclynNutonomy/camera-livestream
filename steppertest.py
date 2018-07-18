fd7fbfb84997dae28605f47c1bad201c7dcbedc3
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-18 15:35:41 +0800

    Initial commit

--- steppertest.py
+++ steppertest.py
@@ -1,23 +1,24 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
-control_pins = [26,19,13,6]
+
+GPIO.setmode(GPIO.BCM)
+control_pins = [22,27,17,4]
 for pin in control_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
 halfstep_seq = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
 ]
 for i in range(512):
-  for halfstep in range(8):
-    for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    for halfstep in range(8):
+        for pin in range(4):
+            GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
 GPIO.cleanup()
