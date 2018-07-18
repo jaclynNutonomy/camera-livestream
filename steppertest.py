40e7f5e46e261fc4a958d240a987c39455cc5884
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-18 16:11:34 +0800

    .

--- steppertest.py
+++ steppertest.py
@@ -1,70 +1,24 @@
 #!/usr/bin/python
-# Import required libraries
-import sys
-import time
 import RPi.GPIO as GPIO
- 
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
- 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [22,27,17,4]
- 
-# Set all pins as output
-for pin in StepPins:
-    print "Setup pins"
-    GPIO.setup(pin,GPIO.OUT)
-    GPIO.output(pin, False)
- 
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
-        
-StepCount = len(Seq)
-StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
- 
-# Read wait time from command line
-if len(sys.argv)>1:
-    WaitTime = int(sys.argv[1])/float(1000)
-else:
-    WaitTime = 10/float(1000)
- 
-# Initialise variables
-StepCounter = 0
- 
-# Start main loop
-while True:
- 
-  print StepCounter,
-  print Seq[StepCounter]
- 
-  for pin in range(0, 4):
-    xpin = StepPins[pin]
-    if Seq[StepCounter][pin]!=0:
-        print " Enable GPIO %i" %(xpin)
-        GPIO.output(xpin, True)
-    else:
-        GPIO.output(xpin, False)
- 
-  StepCounter += StepDir
- 
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount+StepDir
- 
-  # Wait before moving on
-  time.sleep(WaitTime)
+import time
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,13,15]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
+GPIO.cleanup()
