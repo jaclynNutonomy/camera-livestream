1b3f2fd98f6763f1edc78efe11fe93fd7b4c5c26
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-18 16:25:07 +0800

    .

--- steppertest.py
+++ steppertest.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 control_pins = [11,12,13,15]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
