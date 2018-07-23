054e9644e0bb578b5caa60f77018a5cd057a82cd
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-23 14:26:51 +0800

    .

--- websocket-controls.js
+++ websocket-controls.js
@@ -9,14 +9,6 @@ var fs = require('fs'),
 
 var WEBSOCKET_PORT = 8085;
 
-var spawn = require('child_process').spawn;
-var http = require("http");
-var child = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '999999999', '-fps', '20', '-b', '5000000', '-o', '-']);
-var server = http.createServer(function(request, response) {
-  child.stdout.pipe(response);
-});
-server.listen(8085);
-console.log("Server is listening on port 8085");
 
 //robo control socket
 var roboServ = require('http');
@@ -55,6 +47,15 @@ ws.broadcast = function(data, opts) {
 		}
 	});
 };
+
+var spawn = require('child_process').spawn;
+var http = require("http");
+var child = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '999999999', '-fps', '20', '-b', '5000000', '-o', '-']);
+var server = http.createServer(function(request, response) {
+  child.stdout.pipe(response);
+});
+server.listen(8085);
+console.log("Server is listening on port 8085");
 
 ws.connectionCount = 0;
 ws.on('connection', function(socket, upgradeReq) {
