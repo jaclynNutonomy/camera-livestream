5c9a5377b1a69a05e40c3abe24486edd568aa259
jaclynNutonomy <jaclyn@nutonomy.com>
2018-07-26 13:20:11 +0800

    .

--- start_livestream.sh
+++ start_livestream.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 sudo service motion restart
 sudo service motion start
-node camera-livestream/websocket-controls &
+python -m SimpleHTTPServer 8084 &
 python camera-livestream/stepper_con.py 10.101.39.186 &
-python -m SimpleHTTPServer 8084
+node camera-livestream/websocket-controls
 
 #  xterm -title "python" -hold -e python.sh &
 #  xterm -title "brower" -hold -e brower.sh
